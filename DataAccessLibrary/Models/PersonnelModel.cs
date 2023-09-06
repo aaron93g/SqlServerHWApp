446e74dcb5c1a7230c1a5e8fa590482c854c16c4
Aaron Garcia <aaron.m.garcia34@outlook.com>
2023-09-06 00:34:09 -0500

    Created models with properties to to interact with the Db data. Started on a CRUD class.

---
+++ DataAccessLibrary/Models/PersonnelModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Models
+{
+    public class PersonnelModel
+    {
+        // Want to be able to display Employer relation to selected Employee OR
+        // multiple employees relation to selected Employer
+        public List<PersonModel> Employee { get; set; }
+        public List<EmployerModel> Employer { get; set; }
+        public List<EmailModel> EmailAddresses { get; set; }
+    }
+}
---
+++ DataAccessLibrary/SqlCrud.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class SqlCrud
+    {
+        private string connection;
+        SqlDataAccess db = new SqlDataAccess();
+
+        public SqlCrud(string connection)
+        {
+            this.connection = connection;
+        }
+
+
+
+
+    }
+}

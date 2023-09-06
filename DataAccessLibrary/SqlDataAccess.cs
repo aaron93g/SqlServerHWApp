54241a603ecacaeacf3337e1e6b544691c901c9c
Aaron Garcia <aaron.m.garcia34@outlook.com>
2023-09-05 23:17:26 -0500

    message

--- DataAccessLibrary/SqlDataAccess.cs
+++ DataAccessLibrary/SqlDataAccess.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,24 @@ namespace DataAccessLibrary
 {
     public class SqlDataAccess
     {
+
+        public List<T> LoadData<T,U>(string sqlStatement, U parameter, string connectionString)
+        {
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatement, connection).ToList();
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string sqlStatement, T parameter, string connectionString)
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(sqlStatement, parameter);
+            }
+        }
+
     }
 }

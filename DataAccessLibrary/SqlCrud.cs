3dac0bd42eb7fe384966aac89220965a6523f96c
Aaron Garcia <aaron.m.garcia34@outlook.com>
2023-09-07 00:27:58 -0500

    created a READ method using an employer table and personnel table to get names of employees.

--- DataAccessLibrary/SqlCrud.cs
+++ DataAccessLibrary/SqlCrud.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccessLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace DataAccessLibrary
 {
     public class SqlCrud
     {
-        private string connection;
+        private readonly string connection;
         SqlDataAccess db = new SqlDataAccess();
 
         public SqlCrud(string connection)
@@ -17,6 +18,26 @@ namespace DataAccessLibrary
         }
 
 
+        public List<PersonModel> GetAll()
+        {
+            string sqlStatement = "select * from dbo.Person;";
+            return db.LoadData<PersonModel, dynamic>(sqlStatement, new { }, connection);
+        }
+
+        public PersonnelModel GetEmployerInformation(int employerId)
+        {
+            PersonnelModel employer = new PersonnelModel();
+
+
+            string sql = @"select p.*
+                           from dbo.Person p
+                           inner join dbo.Employer e on e.EmployeeId = p.Id
+                           where e.PersonId = @employerId";
+
+            employer.Employee = db.LoadData<PersonModel, dynamic>(sql, new { employerId }, connection);
+
+            return employer;
+        }
 
 
     }
